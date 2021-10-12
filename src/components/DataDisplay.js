ee0b228c96079c28d3fd9720a1dcccde5e8ab199
ed-dickinson <edwardejd@gmail.com>
2021-10-12 23:29:24 +0100

    data display slider

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -2,10 +2,64 @@ import React, {useState, useEffect} from 'react'
 import userService from '../services/user'
 import dateFormatter from '../services/dateFormatter'
 
+const DisplaySelector = ({displayType, setDisplayType}) => {
+  const displayTypes = ['Text', 'Week', 'Fortnight', 'Month', 'None']
+  return (
+    <div className="DisplaySelector">
+      {displayTypes.map(type =>
+        <span
+        className={`${displayType===type&&'selected'}`}
+        onClick={() => setDisplayType(type)}
+        >{type}</span>
+      )}
+    </div>
+  )
+}
+
+const TextDisplay = ({runs, shoes}) => {
+  return(
+    <div>
+      {runs.map(run =>
+        <div key={run.no} className="TextDisplayRun">
+          <span>{dateFormatter.traditionalShort(run.date)}</span>
+          <span>{run.distance} miles</span>
+          <span>{run.elevation} ft</span>
+          <span>{shoes.find(shoe => shoe.no === run.shoe).name} ({run.shoe})</span>
+          <div className="Description"> &nbsp; {run.description}</div>
+        </div>
+
+      )}
+    </div>
+  )
+}
+
+const WeekDisplay = ({runs, shoes}) => {
+  return(
+    <div>
+      {runs.map(run =>
+        <div key={run.no} className="TextDisplayRun">
+          <span>{dateFormatter.traditionalShort(run.date)}</span>
+
+          <span>{run.distance} miles</span>
+          <span>{run.elevation} ft</span>
+          <span>{shoes.find(shoe => shoe.no === run.shoe).name} ({run.shoe})</span>
+          <div className="Description"> &nbsp; {run.description}</div>
+        </div>
+
+      )}
+    </div>
+  )
+}
+
 
 const DataDisplay = ({user, shoes, reRender, setReRender, change, setChange}) => {
 
   const [runs, setRuns] = useState([])
+  const [displayType, setDisplayType] = useState('Text')
+
+  // const handleDisplayTypeChange = () => {
+  //
+  // }
 
   const getRuns = async (userNo) => {
     try {
@@ -20,33 +74,23 @@ const DataDisplay = ({user, shoes, reRender, setReRender, change, setChange}) =>
     }
   }
 
-
-
   useEffect(() => {
     getRuns(user.no)
     // console.log(runs)
 
   }, [user.no, change])
 
-  console.log(shoes)
+  // console.log(shoes)
 
   return (
     <div className="DataDisplay AppBody">
-      Your data:
-      <div>{runs.length} runs total</div>
-      {runs.map(run =>
-        <div key={run.no}>
-          <span>{dateFormatter.traditionalShort(run.date)} </span> —
-          <span>{run.distance} miles</span> —
-          <span>{run.elevation} ft</span> —
-          <span>{shoes.find(shoe => shoe.no === run.shoe).name} ({run.shoe})</span>
-          <div> &nbsp; {run.description}</div>
-        </div>
 
-      )}
-      {runs.forEach(run =>
-        <div>{run.distance}</div>
-      )}
+      <div><strong>Your data:</strong> {runs.length} runs total</div><br />
+      <DisplaySelector displayType={displayType} setDisplayType={setDisplayType} />
+
+      {displayType === "Text" && <TextDisplay runs={runs} shoes={shoes}/>}
+      {displayType === "Week" && <WeekDisplay runs={runs} shoes={shoes}/>}
+
 
     </div>
   )
